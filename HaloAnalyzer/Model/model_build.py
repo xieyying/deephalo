6fcfafd86833f7ab9d2fa5719d47d92a1c772360
xieyying <128657846+xieyying@users.noreply.github.com>
2023-12-07 21:34:58 +0800

    update

--- HaloAnalyzer/Model/model_build.py
+++ HaloAnalyzer/Model/model_build.py
@@ -4,23 +4,24 @@ import tensorflow as tf
 
 def model(input_shape,  output_shape):
     def my_fun(x):
-        # 分割数据
-        data, last_column = tf.split(x, [x.shape[1] - 1, 1], axis=1)
-        data = tf.cast(data,tf.float32)
-
-        # 计算最后一列的10次方
+        data = x[:,:-2]
+        last_column = x[:,-1:]
+        second_last_column = x[:,-2:-1]
         new_last_column = tf.pow(last_column, 10)
-        new_last_column = tf.cast(new_last_column,tf.float32)
-
-        # 合并新的列与原始数据
+        new_second_last_column = tf.pow(second_last_column, 10)
         new_x = tf.concat([data, new_last_column], axis=1)
+        new_x = tf.concat([new_x, new_second_last_column], axis=1)
+
+
         return new_x
 
     """自定义模型结构_单输出"""
     input = keras.Input(shape=(input_shape,), name="features")
     new_input = layers.Lambda(my_fun)(input)
-    share = layers.Dense(128, activation="relu")(new_input)
+    share = layers.Dense(256, activation="relu")(new_input)
     share = layers.Dropout(0.3)(share)
+    share = layers.Dense(128, activation="relu")(new_input)
+    # share = layers.Dropout(0.3)(share)
     share = layers.Dense(64, activation="relu")(share)
     share = layers.Dense(32, activation="relu")(share)
     share = layers.Dense(16, activation="relu")(share)
--- test.py
+++ test.py
@@ -1,59 +1,23 @@
 from molmass import Formula
 import pandas as pd
 from pyteomics import mgf,mzml
+import os
 
-file = r'D:\python\wangmengyuan\dataset\datasets\NPAtlas_download.csv'
-# file = r'D:\python\wangmengyuan\dataset\datasets\uniqueNaturalProduct_formula_from_COCONUT.csv'
-df = pd.read_csv(file,low_memory=False)
-formula = df ["mol_formula"].tolist()
-# formula = df ['Formula'].tolist()
-# 
-br = []
-Cl = []
-for formula in formula:
-    formula_dict = Formula(formula).composition().dataframe().to_dict()['Count']
-    if formula_dict.get('Fe'):
-        if formula_dict.get('Cl') or formula_dict.get('Br') :
-            print(formula)
-        
-#         continue
-#     br.append(formula_dict.get('Br'))
-#     Cl.append(formula_dict.get('Cl'))
-# # 统计不同数量的Br和Cl的数量
-# from collections import Counter
-# br = Counter(br)
-# Cl = Counter(Cl)
-# print(br)
-# print(Cl)
+# path = r"C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset"
 
-# file = r'C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset\base.csv'
-# df = pd.read_csv(file)
+# files = os.listdir(path)
 
-# # 获取group为0-8的数据
-# df = df[df['group']<9]
-# df.to_csv(r'C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset\base1.csv',index=False)
+# for file in files:
+#     if file.endswith('.csv'):
+#         file_path = os.path.join(path,file)
+#         df = pd.read_csv(file_path)
+#         # df['a2_a1'] = df['mz_a2']-df['mz_a1']
+#         # df['a1_a0'] = df['mz_a1']-df['mz_a0']
+#         df['new_a1_a0'] = df['new_a1_mz']-df['new_a0_mz']
+#         df.to_csv(file_path,index=False)
+path = r"C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset\base.csv"
 
-# # 将group改为7
-# df['group'] = 5
-# df.to_csv(r'C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset\Fe.csv',index=False)
-
-# file = r'H:\opendatabase\CASMI_\2022\3_Data-20231206T034508Z-003\3_Data\mzML Data\2_Bonus - Challenges 251-500\pos\A_M1_posPFP_01.mzml'
-
-# spectra = mzml.read(file)
-# s1 = []
-# s2 = []
-# for s in spectra:
-#     if s['ms level'] == 2:
-#         s2.append(s)
-#     elif s['ms level'] == 1:
-#         s1.append(s)
-#     else:
-#         pass
-# print(len(s1))
-# print(len(s2))
-    
-# a = Formula('C70H8OCl3').isotope.mass
-# b = Formula('C70H8OCl3').spectrum()
-# print(a)
-# print(b)
-
+df = pd.read_csv(path)
+#group 0-8
+df = df[df['group']<=8]
+df.to_csv(r"C:\Users\xyy\Desktop\python\HaloAnalyzer_training\0_2_0_test\dataset\base2.csv",index=False)
